c758ecda81c65d395311c4e3f85906e45e14b005
seoyoun-Laptop <seoyoun059@gmail.com>
2022-06-12 02:29:23 +0900

    v2_seoyoun

--- hw5.py
+++ hw5.py
@@ -1,45 +1,80 @@
 #Gradient descent & Newton’s method with the backtracking linesearch
 
 import sys
-
-
-import sys
 import numpy as np
 
 def f(x):
-    return 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
+    return np.array(100*(x[1]-x[0]**2)**2 + (1-x[0])**2)
 
 def grad(x):
     return np.array([400*x[0]**3 - 400*x[0]*x[1] + 2*x[0] - 2, -200*x[0]**2 + 200*x[1]])
 
 def hess(x):
     return np.array([
-        [1200*x[0]**2 - 400*x[1] + 2, -400*x[0]],
-        [-400*x[0], 200]
+        [1200*x[0][0]**2 - 400*x[1][0] + 2, -400*x[0][0]],
+        [-400*x[0][0], 200]
     ])
     
 def norm(x):
-    return (x[0]**2 + x[1]**2)**(1/2)
+    #(x[0]**2 + x[1]**2)**(1/2)
+    return np.linalg.norm(x,2)
 
-#Newton Direction
-def pk(xk):
-    return -(1/hess(xk))*grad(xk)
-   
-def linesearch(xk, f, grad, a0, p, c1, max_ls_iter):
+
+def pk(xk, pk_type):
+    #Newton Direction : pk_type = 0
+    if not pk_type:
+        return -grad(xk)/norm(grad(xk))
+    #Steepest descent direction : pk_type = 1
+    else:
+        return -np.linalg.inv(hess(xk)).dot(grad(xk))
+    
+
+def linesearch(xk, a0, p, c1, max_ls_iter, pk_type):
     a = a0
     ls_iter = 0
-    while ls_iter < max_ls_iter:
-        #if(f(xk + a*pk(xk))<=f(xk) + c1*a*)
-        pass
+    pk_ = pk(xk,pk_type)
     
-def minimization(x0, f, grad, hess, tol, max_iter):
+    while ls_iter < max_ls_iter:
+        if f(xk + a*pk_)<=f(xk) + c1*a*(grad(xk).T.dot(pk_)):
+            break
+        a = p*a
+        ls_iter +=1
+    return a
+
+def minimization(x0, tol, max_iter, pk_type):
     k = 0
     xk = x0
-    print("x0 : " + str(x0) + ", ||grad(f(x0))||_2 : " + str(norm(grad(x0))) )
+    ak = 1.0
     while k < max_iter:
-        #if norm(grad(x)) < tol:
+        if norm(grad(xk)) < tol:
             break
-        
+        print("Iteration %d: alp=%.4e, f=%.4e, gnorm=%.4e"%(k, ak, f(xk), norm(grad(xk))))
+        #Choose a descent direction pk
+        pk_ = pk(xk, pk_type)
+        #Choos ak from direction
+        ak = linesearch(xk, ak, p=0.5, c1=1e-4, max_ls_iter=max_iter/4, pk_type=pk_type)
+        xk += ak*pk_
+        k += 1
     return xk
-        
-    ``````
+
+def main():
+    x1, x2 = map(float, input("input strating point (ex.1.2 1.2) : ").split(' '))
+    print("\n")
+    pk_type = int(input("choose a descent direction method (steepest descent : 0, newtern direction : 1) : "))
+    print("\n")
+    
+    method = ""
+    if not pk_type:
+        method = "steepest descent"
+    else:
+        method = "newtern direction"
+    
+    x = np.array([[x1],[x2]])
+
+    result = minimization(x, tol=1e-12,max_iter=50000, pk_type=pk_type)
+    
+    
+    print("[Method] : "+ method + ", [Result] : (" + str(result[0][0]) + "," + str(result[1][0]) + ")" )
+
+if __name__ == "__main__":
+    main()
